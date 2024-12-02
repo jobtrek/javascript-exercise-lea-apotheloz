9733f1d67648485614f6fe3218f2f78c4f5b9937
lea-apotheloz <apothelozlea2@gmail.com>
2024-12-02 09:14:41 +0100

    feat: finished inputs

--- src/events/inputs.js
+++ src/events/inputs.js
@@ -49,4 +49,12 @@ export function addElementsInListOnEnterKey() {
  */
 export function removeElementsFromListWhenClicked() {
     // Write your code here
+        const list = document.querySelector("#list");
+
+        list.addEventListener('click', (event) => {
+            if (event.target.tagName === 'LI') {
+                event.target.remove();
+            }
+        })
+
 }
